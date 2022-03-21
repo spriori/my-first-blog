968d7e222a047bbd9d260763daf6be08d6697712
spriori <spriori@apsl.net>
2022-03-21 14:26:58 +0100

    quitado el DEBUG

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--1s4m_%8iw9zzco-t2b=og4e*3bsp1uj41q@eekxk+%0%*z_pp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
